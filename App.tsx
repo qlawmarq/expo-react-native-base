ae1489d5457e141e95577374b09c9a6b066bf839
Masa <yoshiiwamasaki@gmail.com>
2022-01-08 19:24:49 +0900

    Remenber me button on sign in page, Introduce Axios setting hooks (#44)
    * introduce expo-image-picker
    
    * add expo-image-picker config to app.json
    
    * add remenber me button to sign-in page, add axios setting hooks ,refactor code

--- App.tsx
+++ App.tsx
@@ -5,7 +5,6 @@ import { SafeAreaProvider } from 'react-native-safe-area-context';
 
 // components & theme
 import { Spinner, NativeBaseProvider } from "native-base"
-import useCachedResources from './hooks/useCachedResources';
 import { theme } from './theme';
 import { useColorScheme } from 'react-native';
 
@@ -17,8 +16,9 @@ import { Provider as ReduxProvider } from 'react-redux';
 import { store, persistor } from './lib/redux/store';
 import { PersistGate } from 'redux-persist/es/integration/react';
 
+import { Root } from './Root';
+
 export default function App() {
-  const isLoadingComplete = useCachedResources();
   const mode = useColorScheme();
   // Changing initialColorMode to 'dark' or 'light' or system(mode)
   theme.config.initialColorMode = mode
@@ -27,15 +27,7 @@ export default function App() {
       <ReduxProvider store={store}>
         <PersistGate loading={<Spinner size="lg" />} persistor={persistor}>
           <SafeAreaProvider>
-          {!isLoadingComplete && 
-              <Spinner size="lg" />
-          }
-          {isLoadingComplete && 
-            <>
-              <Navigation theme={theme} />
-              <StatusBar />
-            </>
-          }
+            <Root theme={theme}/>
           </SafeAreaProvider>
         </PersistGate>
       </ReduxProvider>
